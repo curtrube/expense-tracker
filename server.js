fdb95b60da48d34a8d72f76905d60582317b18bb
Curtis Rubeck <rubeckcurtis@gmail.com>
2023-09-16 15:15:12 -0500

    adds cors policy to allow live server access

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const pg = require("pg");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
+app.use(cors({
+	'origin': 'http://127.0.0.1:5500'
+}));
 app.use(bodyParser.json());
 
 // GET, POST, PUT, DELET
