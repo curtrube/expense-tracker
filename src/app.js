16c4e842b2c091a863e82de7f9670e78de21811a
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-01-12 08:26:38 -0600

    feat: separating routes

--- src/app.js
+++ src/app.js
@@ -3,136 +3,16 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import pg from 'pg';
+import { categories } from './routes/categories.js';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(categories);
 
 app.get("/", (req, res) => {
   res.send("<h1>Expense Tracker</h1>")
-})
-
-app.get("/categories", async (req, res) => {
-  const client = new pg.Client({
-    database: "expense_tracker"
-  });
-  try {
-    await client.connect();
-    console.log('pg client connected')
-    const query = 'SELECT * FROM expense_tracker.categories;'
-    const result = await client.query(query)
-    if (result.rowCount !== 0) {
-      res.status(200).json({ "categories": result.rows })
-    } else {
-      res.json({ "categories": [] })
-    }
-  } catch (error) {
-    console.error('Error querying categories:', error)
-  } finally {
-    await client.end();
-    console.log('pg client disconnected')
-  }
-});
-
-app.get("/categories/:id", async (req, res) => {
-  const client = new pg.Client({
-    database: "expense_tracker"
-  });
-  try {
-    await client.connect();
-    console.log('pg client connected')
-    const categoryId = req.params.id;
-    const query = `SELECT * FROM expense_tracker.categories WHERE category_id = ${categoryId};`;
-    const result = await client.query(query)
-    if (result.rowCount !== 0) {
-      res.status(200).json({ "categories": result.rows })
-    } else {
-      res.json({ "categories": [] })
-    }
-  } catch (error) {
-    console.error('Error querying categories by id:', error)
-  } finally {
-    await client.end();
-    console.log('pg client disconnected')
-  }
-});
-
-app.post("/categories", async (req, res) => {
-  const client = new pg.Client({
-    database: "expense_tracker"
-  });
-  try {
-    await client.connect();
-    console.log('pg client connected')
-    // TODO: input needs to be sanitized
-    // TODO: should we allow duplicate names? probably not
-    const categoryName = req.body.name;
-    const query = `INSERT INTO expense_tracker.categories(name) VALUES('${categoryName}') RETURNING category_id, name;`;
-    const result = await client.query(query)
-    console.log(result)
-    if (result.rowCount !== 0) {
-      res.status(201).json(result.rows)
-    } else {
-      res.json({ "categories": [] })
-    }
-  } catch (error) {
-    console.error('Error querying categories by id:', error)
-  } finally {
-    await client.end();
-    console.log('pg client disconnected')
-  }
-});
-
-app.put("/categories/:id", async(req, res) => {
-  const client = new pg.Client({
-    database: "expense_tracker"
-  });
-  try {
-    await client.connect();
-    console.log('pg client connected');
-    const categoryId = req.params.id;
-    const categoryName = req.body.name;
-    const query = `UPDATE expense_tracker.categories SET name = '${categoryName}' WHERE category_id = '${categoryId}' RETURNING category_id, name;`
-    const result = await client.query(query)
-    if (result.rowCount !== 0) {
-      res.status(201).json(result.rows)
-    } else {
-      res.json({ "categories": [] })
-    }
-  }
-  catch (error) {
-    console.errror("Error updating category id:", error)
-  }
-  finally {
-    await client.end();
-    console.log('pg client disconnected')
-  }
-});
-
-app.delete("/categories/:id", async(req, res) => {
-  const client = new pg.Client({
-    database: "expense_tracker"
-  });
-  try {
-    await client.connect();
-    console.log('pg client connected')
-    const categoryId = req.params.id;
-    const query = `DELETE FROM expense_tracker.categories WHERE category_id = '${categoryId}' RETURNING category_id, name;`
-    const result = await client.query(query)
-    if (result.rowCount !== 0) {
-      res.status(200).json(result.rows)
-    } else {
-      res.json({ "categories": [] })
-    }
-  }
-  catch (error) {
-    console.errror("Error updating category id:", error)
-  }
-  finally {
-    await client.end();
-    console.log('pg client disconnected')
-  }
 })
 
 app.listen(port, () => {
