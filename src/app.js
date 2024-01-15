d69ceb5c78d3ba8773328bd8ae561ff2a906cd25
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-01-15 17:51:29 -0600

    feat: separates routes and controllers

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@
 
 import bodyParser from 'body-parser';
 import express from 'express';
-import pg from 'pg';
+
 import { categories } from './routes/categories.js';
+import { accounts } from './routes/accounts.js'
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
 app.use(categories);
+app.use(accounts)
 
 app.get("/", (req, res) => {
   res.send("<h1>Expense Tracker</h1>")
