c1bbc7f7783fc9985fdbf0cd2a4caa82b9349aea
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-02-27 13:33:00 -0600

    feat: category model and controller

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,130 +1,72 @@
 'use strict';
 
-import pg from 'pg';
-
-console.log('inside controllers/categories')
+import CategoryModel from '../models/categoryModel.js';
 
 export const getCategories = async (req, res) => {
-    console.log('inside getCategories function')
-    const client = new pg.Client({
-        database: "expense_tracker"
-    });
-    try {
-        await client.connect();
-        console.log('pg client connected')
-        const query = 'SELECT * FROM categories;'
-        const result = await client.query(query)
-        if (result.rowCount !== 0) {
-            res.status(200).json({ "categories": result.rows })
-        } else {
-            res.json({ "categories": [] })
-        }
-    } catch (error) {
-        console.error('Error querying categories:', error)
-    } finally {
-        await client.end();
-        console.log('pg client disconnected')
+  const categoryModel = new CategoryModel();
+  try {
+    const results = await categoryModel.findAll();
+    if (results.length !== 0) {
+      res.status(200).json({ categories: results });
+    } else {
+      res.status(204).json({ categories: [] });
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getCategory = async (req, res) => {
-    const client = new pg.Client({
-        database: "expense_tracker"
-    });
-    try {
-        await client.connect();
-        console.log('pg client connected')
-        const categoryId = req.params.id;
-        const query = `SELECT * FROM categories WHERE category_id = ${categoryId};`;
-        const result = await client.query(query)
-        if (result.rowCount !== 0) {
-            res.status(200).json({ "categories": result.rows })
-        } else {
-            res.json({ "categories": [] })
-        }
-    } catch (error) {
-        console.error('Error querying categories by id:', error)
-    } finally {
-        await client.end();
-        console.log('pg client disconnected')
+  const categoryId = req.params.id;
+  const categoryModel = new CategoryModel();
+  try {
+    const results = await categoryModel.findOne(categoryId);
+    if (results.length !== 0) {
+      res.status(200).json({ categories: results });
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const createCategory = async (req, res) => {
-    const client = new pg.Client({
-        database: "expense_tracker"
-    });
-    try {
-        await client.connect();
-        console.log('pg client connected')
-        // TODO: input needs to be sanitized
-        // TODO: should we allow duplicate names? probably not
-        const categoryName = req.body.name;
-        console.log(categoryName)
-        const query = `INSERT INTO categories(name) VALUES('${categoryName}') RETURNING category_id, name;`;
-        console.log(`Executing Query: ${query}`)
-        const result = await client.query(query)
-        console.log(result)
-        if (result.rowCount !== 0) {
-            res.status(201).json(result.rows)
-        } else {
-            res.json({ "categories": [] })
-        }
-    } catch (error) {
-        console.error('Error querying categories by id:', error)
-    } finally {
-        await client.end();
-        console.log('pg client disconnected')
+  // TODO: input needs to be sanitized
+  // TODO: should we allow duplicate names? probably not
+  const categoryName = req.body.name;
+  const categoryModel = new CategoryModel();
+  try {
+    const results = await categoryModel.create(categoryName);
+    if (results.length !== 0) {
+      res.status(201).json({ categories: results });
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const updateCategory = async (req, res) => {
-    const client = new pg.Client({
-        database: "expense_tracker"
-    });
+    const categoryId = req.params.id;
+    const categoryName = req.body.name;
+    const categoryModel = new CategoryModel();
     try {
-        await client.connect();
-        console.log('pg client connected');
-        const categoryId = req.params.id;
-        const categoryName = req.body.name;
-        const query = `UPDATE categories SET name = '${categoryName}' WHERE category_id = '${categoryId}' RETURNING category_id, name;`
-        const result = await client.query(query)
-        if (result.rowCount !== 0) {
-            res.status(201).json(result.rows)
-        } else {
-            res.json({ "categories": [] })
+        const results = await categoryModel.update(categoryId, categoryName);
+        if (results.length !== 0) {
+            res.status(201).json({ categories: results })
         }
-    }
-    catch (error) {
-        console.errror("Error updating category id:", error)
-    }
-    finally {
-        await client.end();
-        console.log('pg client disconnected')
+    } catch(error) {
+        console.log(error)
     }
 };
 
 export const deleteCategory = async (req, res) => {
-    const client = new pg.Client({
-        database: "expense_tracker"
-    });
+    const categoryId = req.params.id;
+    const categoryModel = new CategoryModel();
     try {
-        await client.connect();
-        console.log('pg client connected')
-        const categoryId = req.params.id;
-        const query = `DELETE FROM categories WHERE category_id = '${categoryId}' RETURNING category_id, name;`
-        const result = await client.query(query)
-        if (result.rowCount !== 0) {
-            res.status(200).json(result.rows)
-        } else {
-            res.json({ "categories": [] })
+        const results = await categoryModel.delete(categoryId);
+        if (results.length !== 0) {
+            res.status(202).json({ categories: results })
         }
-    }
-    catch (error) {
-        console.errror("Error updating category id:", error)
-    }
-    finally {
-        await client.end();
-        console.log('pg client disconnected')
+    } catch(error) {
+        console.log(error)
     }
 };
---
+++ src/models/categoryModel.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import dbService from '../db/db-service.js';
+
+class CategoryModel {
+  findAll = async () => {
+    const sql = 'SELECT * FROM categories;';
+    return await dbService.query(sql);
+  };
+
+  findOne = async (categoryId) => {
+    const sql = `SELECT * FROM categories WHERE category_id = ${categoryId};`;
+    return await dbService.query(sql);
+  };
+
+  create = async (categoryName) => {
+    const sql = `
+        INSERT INTO categories(name)
+        VALUES('${categoryName}')
+        RETURNING category_id, name;
+        `;
+    return await dbService.query(sql);
+  };
+
+  update = async (categoryId, categoryName) => {
+    const sql = `
+            UPDATE categories
+            SET name = '${categoryName}'
+            WHERE category_id = '${categoryId}'
+            RETURNING category_id, name;
+            `;
+    return await dbService.query(sql);
+  };
+
+  delete = async (categoryId) => {
+    const sql = `
+        DELETE FROM categories
+        WHERE category_id = '${categoryId}'
+        RETURNING category_id, name;
+        `;
+    return await dbService.query(sql);
+  };
+}
+
+export default CategoryModel;
