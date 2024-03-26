37c35a61d389a5f7f3a02961f19053d537fffa7a
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-03-26 08:45:25 -0500

    feat: health check

--- src/routes/health.js
+++ src/routes/health.js
@@ -2,8 +2,19 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/alive', (req, res) => {
-  res.status(200).send('alive');
+router.get('/health', async (req, res) => {
+  const date = new Date();
+  const healthCheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: date.toISOString(),
+  };
+  try {
+    res.send(healthCheck);
+  } catch (err) {
+    healthCheck.message = err;
+    res.status(503);
+  }
 });
 
 export default router;
