ead51df41ff57fa5cfe5919e07769adc0d1d3457
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-07-03 11:13:57 -0500

    feat: use eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslint from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin-js';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/semi': ['error', 'always'],
+      'sort-imports': 'error',
+      'no-unused-vars': 'warn',
+    },
+  },
+];
