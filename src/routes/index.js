e03695e63cae17a29297f72e57ff8fa8b2c72dc3
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-07-06 17:45:45 -0500

    fix: import user router

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,14 +7,14 @@ import categoryRouter from './categoryRoutes.js';
 // import errorRouter from './error.js';
 import healthRouter from './health.js';
 import transactionsRouter from './transactions.js';
-import usersRouter from './users.js';
+import userRouter from './userRoutes.js';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 // router.use('/', errorRouter);
 router.use('/', healthRouter);
-router.use('/', authenticate, usersRouter);
+router.use('/', authenticate, userRouter);
 router.use('/', authenticate, transactionsRouter);
 router.use('/', authenticate, categoryRouter);
 router.use('/', authenticate, accountRouter);
